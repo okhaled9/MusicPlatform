2ddb8dde41a4347efa5634e205f45c2a0c3bf0c1
omar khaled <okhaled9@gmail.com>
2023-10-09 14:07:28 +0300

    added pillow and approved_albums as porperty

--- musicplatform/artists/models.py
+++ musicplatform/artists/models.py
@@ -1,20 +1,13 @@
 from django.db import models
-from django.db.models import Count, Q
-
-
-class ArtistManager(models.Manager):
-    def get_queryset(self):
-        return (
-            super()
-            .get_queryset()
-            .annotate(approved_albums=Count("album", filter=Q(album__is_approved=True)))
-        )
 
 
 class Artist(models.Model):
     stage_name = models.CharField(max_length=50, unique=True)
     social_link = models.URLField(blank=True)
-    objects = ArtistManager()
+
+    @property
+    def approved_albums(self):
+        return self.album_set.filter(is_approved=True).count()
 
     def __str__(self):
         return self.stage_name
